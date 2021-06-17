fec8ef86112d31ae87f70203db7c77927eef1734
yishairasowsky <yishairasowsky@gmail.com>
2021-06-17 20:29:50 +0300

    remove private header

--- generate_token.py
+++ generate_token.py
@@ -6,9 +6,10 @@ data = 'grant_type=client_credentials'
 
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'OWVjZmIxNmUtMmIwZi00ZWNjLWI0YTEtNTY0NGY0ZjFjOThhOjQ0OGE0MjNmLWNiOGEtNGNkNS1iNmVlLTc3OTRmMDFkOTNjMQ'
     }
 
 x = requests.post(url=url, data=data, headers=headers)
 
-print(x)
+print(x.json())
+
+pass
