baf5a9501fe3cc2c6b6cd97689bb3705f36b1eda
yishairasowsky <yishairasowsky@gmail.com>
2021-07-05 17:54:48 +0300

    keep only input data that seems necessary

--- generate_token.py
+++ generate_token.py
@@ -1,15 +1,17 @@
+# Attempt to implement this https://api.lulu.com/docs/#section/Getting-Started/Generate-a-Token
 import requests
-
+from private_info import Client_key, Client_secret, Base64 # obtained from https://api.sandbox.lulu.com/user-profile/api-keys
 url = 'https://api.lulu.com/auth/realms/glasstree/protocol/openid-connect/token'
-
-data = 'grant_type=client_credentials'
-
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
+data = {
+    "grant_type":"client_credentials",
     }
-
-x = requests.post(url=url, data=data, headers=headers)
-
-print(x.json())
-
-pass
+params = {
+    'client_key':Client_key,
+    'client_secret':Client_secret,
+}
+headers = {
+    'Content-Type':'application/x-www-form-urlencoded',
+    'Authorization':Base64,
+    }
+r = requests.post(url=url, data=data, headers=headers,params=params)
+print(r.json())
