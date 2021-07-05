1b4f7ac3d025a3c6eab17dce106c3d0c86c65ff8
yishairasowsky <yishairasowsky@gmail.com>
2021-07-05 18:58:46 +0300

    change url to include sandbox

--- generate_token.py
+++ generate_token.py
@@ -1,17 +1,10 @@
 # Attempt to implement this https://api.lulu.com/docs/#section/Getting-Started/Generate-a-Token
 import requests
-from private_info import Client_key, Client_secret, Base64 # obtained from https://api.sandbox.lulu.com/user-profile/api-keys
-url = 'https://api.lulu.com/auth/realms/glasstree/protocol/openid-connect/token'
-data = {
-    "grant_type":"client_credentials",
-    }
-params = {
-    'client_key':Client_key,
-    'client_secret':Client_secret,
-}
-headers = {
-    'Content-Type':'application/x-www-form-urlencoded',
-    'Authorization':Base64,
-    }
-r = requests.post(url=url, data=data, headers=headers,params=params)
-print(r.json())
+from private_info import Base64 # obtained from https://api.sandbox.lulu.com/user-profile/api-keys
+
+url = 'https://api.sandbox.lulu.com/auth/realms/glasstree/protocol/openid-connect/token'
+data = {"grant_type":"client_credentials"}
+headers = {'Authorization':Base64}
+response = requests.post(url=url, data=data, headers=headers)
+json_data = response.json()
+print(json_data)
