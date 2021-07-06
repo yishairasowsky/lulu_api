6c82c796db1c87771b6ebf17891cb0ce3eb757d9
yishairasowsky <yishairasowsky@gmail.com>
2021-07-06 18:43:32 +0300

    add end point for print jobs

--- generate_token.py
+++ generate_token.py
@@ -1,10 +1,22 @@
 # Attempt to implement this https://api.lulu.com/docs/#section/Getting-Started/Generate-a-Token
+# For help use
+#  https://github.com/minireference/lulu-api-client/blob/358395d21685245f75a60094c2f7d80b6b3dddb1/luluapi.py#L67
 import requests
 from private_info import Base64 # obtained from https://api.sandbox.lulu.com/user-profile/api-keys
 
-url = 'https://api.sandbox.lulu.com/auth/realms/glasstree/protocol/openid-connect/token'
+SANDBOX_BASE_URL = "https://api.sandbox.lulu.com/"
+TOKEN_ENDPOINT = SANDBOX_BASE_URL + "auth/realms/glasstree/protocol/openid-connect/token"
+
+# url = 'https://api.sandbox.lulu.com/auth/realms/glasstree/protocol/openid-connect/token'
 data = {"grant_type":"client_credentials"}
 headers = {'Authorization':Base64}
-response = requests.post(url=url, data=data, headers=headers)
-json_data = response.json()
-print(json_data)
+post_response = requests.post(url=TOKEN_ENDPOINT, data=data, headers=headers)
+post_json_data = post_response.json()
+access_token = post_json_data['access_token']
+
+Auth = f'Bearer {access_token}'
+headers = {'Authorization':Auth, 'Content-Type':'application/json'}
+TEST_ENDPOINT = SANDBOX_BASE_URL + "print-jobs/"  
+get_response = requests.get(url=TEST_ENDPOINT, headers=headers)
+get_json_data = get_response.json()
+print(get_json_data)
